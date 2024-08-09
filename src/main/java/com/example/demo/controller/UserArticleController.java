35f6eafbbf7e2baf718cab71afdb12d183897493
hrjo0120 <hrjo0120@gmail.com>
2024-08-09 14:15:36 +0900

    게시글 테스트 데이터 생성

--- src/main/java/com/example/demo/controller/UserArticleController.java
+++ src/main/java/com/example/demo/controller/UserArticleController.java
@@ -14,28 +14,44 @@ public class UserArticleController {
 
 	int lastArticleId = 0;
 	static List<Article> articles;
-	
+
 	public UserArticleController() {
 		articles = new ArrayList<>();
 
+		// 게시글 테스트 데이터 생성
+		makeTestData();
 	}
-	
-	@RequestMapping("/usr/article/doAdd")
-	@ResponseBody
-	public Article doAdd(String title, String body) {
+
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목" + i;
+			String body = "내용" + i;
+
+			writeArticle(title, body);
+		}
+	}
+
+	private Article writeArticle(String title, String body) {
 		int id = lastArticleId + 1;
 		Article article = new Article(id, title, body);
 		articles.add(article);
 		lastArticleId++;
-		
 		return article;
 	}
-	
+
+	@RequestMapping("/usr/article/doAdd")
+	@ResponseBody
+	public Article doAdd(String title, String body) {
+		Article article = writeArticle(title, body);
+
+		return article;
+	}
+
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() {
-		
+
 		return articles;
 	}
-	
+
 }
