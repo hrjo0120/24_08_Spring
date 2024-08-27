f30ee3266827421fb5bbcfb2320284898966aeef
hrjo0120 <hrjo0120@gmail.com>
2024-08-27 14:21:25 +0900

    댓글 테스트데이터를 article detail 에 표시, 관련 구조 추가

--- src/main/java/com/example/demo/controller/UserArticleController.java
+++ src/main/java/com/example/demo/controller/UserArticleController.java
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.demo.service.ArticleService;
 import com.example.demo.service.BoardService;
 import com.example.demo.service.ReactionPointService;
+import com.example.demo.service.ReplyService;
 import com.example.demo.util.Ut;
 import com.example.demo.vo.Article;
 import com.example.demo.vo.Board;
+import com.example.demo.vo.Reply;
 import com.example.demo.vo.ResultData;
 import com.example.demo.vo.Rq;
 
@@ -32,9 +34,12 @@ public class UserArticleController {
 
 	@Autowired
 	private BoardService boardService;
-	
+
 	@Autowired
 	private ReactionPointService reactionPointService;
+
+	@Autowired
+	private ReplyService replyService;
 
 	@RequestMapping("/usr/article/write")
 	public String showWrite(HttpServletRequest req) {
@@ -190,8 +195,16 @@ public class UserArticleController {
 		if (usersReactionRd.isSuccess()) {
 			model.addAttribute("userCanMakeReaction", usersReactionRd.isSuccess());
 		}
-				
+		
+		List<Reply> replies = replyService.getForPrintReplies("article", id);
+
+		int repliesCount = replies.size();
+
 		model.addAttribute("article", article);
+		
+		model.addAttribute("replies", replies);
+		model.addAttribute("repliesCount", repliesCount);
+		
 		model.addAttribute("isAlreadyAddGoodRp",
 				reactionPointService.isAlreadyAddGoodRp(rq.getLoginedMemberId(), id, "article"));
 		model.addAttribute("isAlreadyAddBadRp",
