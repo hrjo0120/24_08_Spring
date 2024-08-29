a9b2cfa97eba76f400343ae141872ac0ded6f1cd
hrjo0120 <hrjo0120@gmail.com>
2024-08-29 15:32:04 +0900

    회원정보 수정 시나리오

--- src/main/java/com/example/demo/controller/UserMemberController.java
+++ src/main/java/com/example/demo/controller/UserMemberController.java
@@ -123,4 +123,23 @@ public class UserMemberController {
 		return "usr/member/checkPw";
 	}
 	
+	@RequestMapping("/usr/member/doCheckPw")
+	@ResponseBody
+	public String doCheckPw(String loginPw) {
+		if (Ut.isEmptyOrNull(loginPw)) {
+			return Ut.jsHistoryBack("F-1", "비번 써");
+		}
+
+		if (rq.getLoginedMember().getLoginPw().equals(loginPw) == false) {
+			return Ut.jsHistoryBack("F-2", "비번 틀림");
+		}
+
+		return Ut.jsReplace("S-1", Ut.f("비밀번호 확인 성공"), "modify");
+	}
+
+	@RequestMapping("/usr/member/modify")
+	public String showmyModify() {
+		return "usr/member/modify";
+	}
+	
 }
