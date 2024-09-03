a369940f3f49c84a8b332c54acf4df44eda6051d
hrjo0120 <hrjo0120@gmail.com>
2024-09-03 16:51:22 +0900

    기존 비번 암호화, 회원가입, 로그인, 회원정보 수정, 비번 체크시 암호화, 로그인 아이디 찾기 구현

--- src/main/java/com/example/demo/util/Ut.java
+++ src/main/java/com/example/demo/util/Ut.java
@@ -4,6 +4,8 @@ import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
@@ -90,7 +92,7 @@ public class Ut {
 	public static String f(String format, Object... args) {
 		return String.format(format, args);
 	}
-	
+
 	public static String getEncodedCurrentUri(String currentUri) {
 		try {
 			return URLEncoder.encode(currentUri, "UTF-8");
@@ -99,7 +101,7 @@ public class Ut {
 			return currentUri;
 		}
 	}
-	
+
 	public static <T> T ifEmpty(T data, T defaultValue) {
 		if (isEmpty(data)) {
 			return defaultValue;
@@ -273,5 +275,26 @@ public class Ut {
 
 	public static <T> T reqAttr(HttpServletRequest req, String attrName, T defaultValue) {
 		return (T) ifNull(req.getAttribute(attrName), defaultValue);
+	}
+
+	// sha256
+	public static String sha256(String input) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			byte[] hash = md.digest(input.getBytes("UTF-8"));
+			StringBuffer hexString = new StringBuffer();
+
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if (hex.length() == 1)
+					hexString.append('0');
+				hexString.append(hex);
+			}
+
+			return hexString.toString();
+		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
